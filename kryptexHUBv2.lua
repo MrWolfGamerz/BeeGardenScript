1fa54f65ef1ef9a8aa47466d715f973fc0dce107
MrWolfGamerz <38703951+MrWolfGamerz@users.noreply.github.com>
2026-06-18 22:24:32 -0400

    Update kryptexHUBv2.lua

--- kryptexHUBv2.lua
+++ kryptexHUBv2.lua
@@ -20,7 +20,7 @@ local settings = {
 	UFOName = "UFO",
 	GhostNames = { "FastGhost", "NormalGhost", "SlowGhost" },
 	GhostPrefixes = { "FastGhost_", "NormalGhost_", "SlowGhost_" },
-	MaterialNames = { "Meteoron", "Snowflake","CandyPickup", "PumpkinPickup", "Candy", "SnowflakePickup", "MeteoronPickup", "AlienPickup", "NeonPickup" },
+	MaterialNames = { "Meteoron", "Snowflake","CandyPickup", "PumpkinPickup", "Candy", "SnowflakePickup", "MeteoronPickup", "AlienPickup" },
 	DigPileName = "DigPile",
 
 	TeleportHeight = 4,
